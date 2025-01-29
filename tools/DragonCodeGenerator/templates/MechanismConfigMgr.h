50a0f69d5a129675bf33b832be53352ea00b8456
kphan20 <knpmths@gmail.com>
2025-01-28 19:44:57 -0500

    made dedicated RobotIdentifier and updated config

--- tools/DragonCodeGenerator/templates/MechanismConfigMgr.h
+++ tools/DragonCodeGenerator/templates/MechanismConfigMgr.h
@@ -8,15 +8,6 @@ $$_GEN_NOTICE_$$
 class MechanismConfigMgr
 {
 public:
-    enum RobotIdentifier
-    {
-        CHASSISBOT_9998 = 9998,
-        CHASSIS_BOT_9997 = 9997,
-        $$_ROBOT_CONFIGURATIONS_NAMES_ENUMS_$$
-
-            MAX_ROBOT_IDENTIFIERS
-    };
-
     static MechanismConfigMgr *GetInstance();
     MechanismConfig *GetCurrentConfig() const { return m_config; }
     void InitRobot(RobotIdentifier);
