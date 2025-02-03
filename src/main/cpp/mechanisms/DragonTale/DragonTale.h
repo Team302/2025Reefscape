c5eded2fc77d12f2950893dbdc5372b216185a7b
Tan560 <97492534+Tan560@users.noreply.github.com>
2025-02-03 00:15:27 -0500

    Updating from testing today, forgot to update

--- src/main/cpp/mechanisms/DragonTale/DragonTale.h
+++ src/main/cpp/mechanisms/DragonTale/DragonTale.h
@@ -205,7 +205,7 @@ private:
 	const units::length::inch_t m_minHeight{0.0};
 	const units::length::inch_t m_maxHeight{30.0};
 
-	const units::length::inch_t m_elevatorErrorThreshold{5.0};
+	const units::length::inch_t m_elevatorErrorThreshold{4.0};
 	const units::length::inch_t m_elevatorProtectionHeight{5.0};
 	const units::angle::degree_t m_armProtectionAngle{70.0};
 
