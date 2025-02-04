bf7b96ea769d46f8e4d24f458175f739ddbfe2c3
reslip <reslip@icloud.com>
2025-02-03 22:46:41 -0500

    fix pragma once

--- src/main/cpp/vision/DragonVisionStructLogger.cpp
+++ src/main/cpp/vision/DragonVisionStructLogger.cpp
@@ -12,7 +12,6 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 // OR OTHER DEALINGS IN THE SOFTWARE.
 //====================================================================================================================================================
-#pragma once
 
 #include "vision/DragonVisionStructLogger.h"
 #include "utils/logging/Logger.h"
--- src/main/cpp/vision/DragonVisionStructs.h
+++ src/main/cpp/vision/DragonVisionStructs.h
@@ -12,6 +12,7 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 // OR OTHER DEALINGS IN THE SOFTWARE.
 //====================================================================================================================================================#pragma once
+#pragma once
 
 #include "frc/geometry/Pose3d.h"
 #include "frc/geometry/Translation3d.h"
