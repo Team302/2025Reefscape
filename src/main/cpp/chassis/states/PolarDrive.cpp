c0a1057ad58c44bfa20778b554800b08dd45a82a
Tan560 <97492534+Tan560@users.noreply.github.com>
2025-02-10 18:57:31 -0500

    Polar Drive works

--- src/main/cpp/chassis/states/PolarDrive.cpp
+++ src/main/cpp/chassis/states/PolarDrive.cpp
@@ -32,84 +32,65 @@ using std::string;
 PolarDrive::PolarDrive(RobotDrive *robotDrive) : RobotDrive(robotDrive->GetChassis()),
                                                  m_robotDrive(robotDrive)
 {
+    frc::DriverStation::Alliance allianceColor = FMSData::GetInstance()->GetAllianceColor();
+    m_reefCenter = allianceColor == frc::DriverStation::Alliance::kRed ? frc::Pose3d{FieldConstants::GetInstance()->GetFieldElementPose(FieldConstants::FIELD_ELEMENT::RED_REEF_CENTER)} /*load red reef*/ : frc::Pose3d{FieldConstants::GetInstance()->GetFieldElementPose(FieldConstants::FIELD_ELEMENT::BLUE_REEF_CENTER)};
+}
+
+void PolarDrive::Init(ChassisMovement &chassismovement)
+{
+    if (m_chassis != nullptr)
+    {
+        frc::Pose2d currentPose = m_chassis->GetPose();
+        units::length::meter_t xDiff = currentPose.X() - m_reefCenter.X();
+        units::length::meter_t yDiff = currentPose.Y() - m_reefCenter.Y();
+
+        units::length::meter_t m_radius = units::math::hypot(xDiff, yDiff);
+    }
 }
 
 std::array<frc::SwerveModuleState, 4> PolarDrive::UpdateSwerveModuleStates(ChassisMovement &chassisMovement)
 {
-
-    /*
-    frc::Pose3d reefCenter = FMSData::GetInstance()->GetAllianceColor() == frc::DriverStation::Alliance::kRed
-                                 ? frc::Pose3d{FieldConstants::GetInstance()->GetFieldElement(FieldConstants::FIELD_ELEMENT::RED_REEF_CENTER)}
-                                 : frc::Pose3d{FieldConstants::GetInstance()->GetFieldElement(FieldConstants::FIELD_ELEMENT::BLUE_REEF_CENTER)};
-
-    frc::Pose2d currentPose = m_chassis->GetPose();
-    units::length::meter_t xDiff = currentPose.X() - reefCenter.X();  //Still useful for logging
-    units::length::meter_t yDiff = currentPose.Y() - reefCenter.Y(); //Still useful for logging
-    units::length::meter_t radius = units::math::hypot(xDiff, yDiff);
-    units::angle::degree_t angle = units::math::atan2(yDiff, xDiff);
-
-    auto chassisSpeeds = chassisMovement.chassisSpeeds;
-
-    double radialInput = chassisSpeeds.vx.value();
-    double radialVelocity = radialInput; // Scale as needed
-
-    double angularInput = chassisSpeeds.vy.value();
-    double angularVelocity = (radius.value() > 0.01) ? angularInput / radius.value() : 0.0;
-    angle += units::angle::degree_t(angularVelocity * m_loopRate);
-
-    // Calculate the *resulting* desired velocities in field coordinates
-    chassisSpeeds.vx = units::velocity::meters_per_second_t(radialVelocity * units::math::cos(angle).value() - radius.value() * angularVelocity * units::math::sin(angle).value());
-    chassisSpeeds.vy = units::velocity::meters_per_second_t(radialVelocity * units::math::sin(angle).value() + radius.value() * angularVelocity * units::math::cos(angle).value());
-
-
-    auto rot2d = Rotation2d(m_chassis->GetYaw());
-    chassisMovement.chassisSpeeds = ChassisSpeeds::FromFieldRelativeSpeeds(chassisSpeeds.vx, chassisSpeeds.vy, chassisSpeeds.omega, rot2d);
-*/
     if (m_chassis != nullptr)
     {
-        frc::Pose3d reefCenter = frc::Pose3d{};
-        frc::DriverStation::Alliance allianceColor = FMSData::GetInstance()->GetAllianceColor();
-        reefCenter = allianceColor == frc::DriverStation::Alliance::kRed ? frc::Pose3d{FieldConstants::GetInstance()->GetFieldElement(FieldConstants::FIELD_ELEMENT::RED_REEF_CENTER)} /*load red reef*/ : frc::Pose3d{FieldConstants::GetInstance()->GetFieldElement(FieldConstants::FIELD_ELEMENT::BLUE_REEF_CENTER)};
         auto chassisSpeeds = chassisMovement.chassisSpeeds;
 
         frc::Pose2d currentPose = m_chassis->GetPose();
         Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "AlignDebugging", "Y", currentPose.Y().value());
         Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "AlignDebugging", "X", currentPose.X().value());
-        units::length::meter_t xDiff = currentPose.X() - reefCenter.X();
-        units::length::meter_t yDiff = currentPose.Y() - reefCenter.Y();
+        units::length::meter_t xDiff = currentPose.X() - m_reefCenter.X();
+        units::length::meter_t yDiff = currentPose.Y() - m_reefCenter.Y();
 
-        units::length::meter_t radius = units::math::hypot(xDiff, yDiff);
         units::angle::degree_t angle = units::math::atan2(yDiff, xDiff);
 
         // Radial velocity: Changes radius
         double radialVelocity = chassisSpeeds.vx.value(); // Forward/backward motion directly affects radius
-        if (radialVelocity > 0.1)
+        if (radialVelocity > 0.25)
         {
-            radius += units::length::meter_t(radialVelocity * m_loopRate * -1.0); // Negative since forward decreases radius
+            m_radius += units::length::meter_t(radialVelocity * m_loopRate); // Negative since forward decreases radius
         }
 
         // Angular velocity: Changes angle
-        double angularVelocity = chassisSpeeds.vy.value() / radius.value(); // Clockwise/counter-clockwise motion
+        double angularVelocity = chassisSpeeds.vy.value() / m_radius.value(); // Clockwise/counter-clockwise motion
         angle += units::angle::degree_t(angularVelocity * m_loopRate);
 
         Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "AlignDebugging", "radialVelocity", radialVelocity);
         Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "AlignDebugging", "angularVelocity", angularVelocity);
 
         // Convert polar velocities back to Cartesian
-        double vxNew = radialVelocity * units::math::cos(angle).value() - (radius.value() * angularVelocity * units::math::sin(angle).value());
-        double vyNew = radialVelocity * units::math::sin(angle).value() + (radius.value() * angularVelocity * units::math::cos(angle).value());
+        double vxNew = radialVelocity * units::math::cos(angle).value() - (m_radius.value() * angularVelocity * units::math::sin(angle).value());
+        double vyNew = radialVelocity * units::math::sin(angle).value() + (m_radius.value() * angularVelocity * units::math::cos(angle).value());
 
-        Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "AlignDebugging", "Raidus", radius.value());
+        Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "AlignDebugging", "Raidus", m_radius.value());
         Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "AlignDebugging", "Angle", angle.value());
 
-        chassisSpeeds.vx = units::velocity::meters_per_second_t(vxNew);
-        chassisSpeeds.vy = units::velocity::meters_per_second_t(vyNew);
+        chassisSpeeds.vx = units::velocity::meters_per_second_t(-vxNew);
+        chassisSpeeds.vy = units::velocity::meters_per_second_t(-vyNew);
 
         Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "AlignDebugging", "VxNew", vxNew);
         Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "AlignDebugging", "VyNew", vyNew);
         auto rot2d = Rotation2d(m_chassis->GetYaw());
-        chassisMovement.chassisSpeeds = ChassisSpeeds::FromFieldRelativeSpeeds(chassisMovement.chassisSpeeds.vx,
-                                                                               chassisMovement.chassisSpeeds.vy,
+        chassisMovement.chassisSpeeds = ChassisSpeeds::FromFieldRelativeSpeeds(chassisSpeeds.vx,
+                                                                               chassisSpeeds.vy,
                                                                                chassisMovement.chassisSpeeds.omega,
                                                                                rot2d);
         return m_robotDrive->UpdateSwerveModuleStates(chassisMovement);
@@ -125,8 +106,4 @@ std::array<frc::SwerveModuleState, 4> PolarDrive::UpdateSwerveModuleStates(Chass
 std::string PolarDrive::GetDriveStateName() const
 {
     return std::string("PolarDrive");
-}
-
-void PolarDrive::Init(ChassisMovement &chassisMovement)
-{
 }
--- src/main/cpp/chassis/states/PolarDrive.h
+++ src/main/cpp/chassis/states/PolarDrive.h
@@ -37,4 +37,6 @@ public:
 private:
     RobotDrive *m_robotDrive;
     double m_loopRate = 0.02;
+    units::length::meter_t m_radius{5.0};
+    frc::Pose3d m_reefCenter;
 };
