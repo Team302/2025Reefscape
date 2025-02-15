9f059c9b44bcfb325ce2dc211403a0812e7b9b91
Tan560 <97492534+Tan560@users.noreply.github.com>
2025-02-15 15:39:26 -0500

    Fixed DragonTale Warning

--- src/main/cpp/mechanisms/DragonTale/DragonTale.cpp
+++ src/main/cpp/mechanisms/DragonTale/DragonTale.cpp
@@ -760,7 +760,6 @@ void DragonTale::SetSensorFailSafe()
 
 units::length::inch_t DragonTale::GetAlgaeHeight()
 {
-	frc::DriverStation::Alliance allianceColor = FMSData::GetInstance()->GetAllianceColor();
 	// Adjust the angle to the nearest 60-degree increment
 	auto info = (DragonTargetFinder::GetInstance()->GetPose(DragonTargetFinderTarget::CLOSEST_REEF_ALGAE));
 	if (info)
