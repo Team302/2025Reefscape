28dde8f6a34f4adfbc69b8f7ada4ea06f2c8eb3f
Alyssa <Frank0624789@gmail.com>
2025-02-19 20:37:30 -0500

    Added logging know what the error is

--- src/main/cpp/chassis/states/PolarDrive.cpp
+++ src/main/cpp/chassis/states/PolarDrive.cpp
@@ -36,6 +36,9 @@ PolarDrive::PolarDrive(RobotDrive *robotDrive) : RobotDrive(robotDrive->GetChass
     {
         auto info = finder->GetPose(DragonTargetFinderTarget::REEF_CENTER);
         m_reefCenter = get<1>(info.value());
+        Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "polarlog", "reefposeX", m_reefCenter.X().value());
+        Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "polarlog", "reefposeY", m_reefCenter.Y().value());
+        // Delete this logging once it works
     }
 }
 
@@ -48,6 +51,8 @@ void PolarDrive::Init(ChassisMovement &chassismovement)
         units::length::meter_t yDiff = currentPose.Y() - m_reefCenter.Y();
 
         m_radiusTarget = units::math::hypot(xDiff, yDiff);
+        Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "polarlog", "radTarget", m_radiusTarget.value());
+        // Delete this logging once it works
     }
 }
 
@@ -63,6 +68,9 @@ std::array<frc::SwerveModuleState, 4> PolarDrive::UpdateSwerveModuleStates(Chass
         units::length::meter_t yDiff = currentPose.Y() - m_reefCenter.Y();
 
         units::angle::degree_t angle = units::math::atan2(yDiff, xDiff);
+
+        Logger::GetLogger()->LogData(LOGGER_LEVEL::PRINT, "polarlog", "Logatan", angle.value());
+        // Delete this logging once it works
 
         double radialVelocity = abs((chassisSpeeds.vx / m_chassis->GetMaxSpeed()).value()) > 0.25 ? chassisSpeeds.vx.value() : 0;
         m_radiusTarget += units::length::meter_t(radialVelocity * m_loopRate);
