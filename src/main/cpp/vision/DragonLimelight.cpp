8da5ecc183872184386d161cac64d7a637543742
Aden25 <benjamin.sameck@stu.lok12.org>
2025-02-22 10:54:31 -0500

    deleted almost all memory o last year's code

--- src/main/cpp/vision/DragonLimelight.cpp
+++ src/main/cpp/vision/DragonLimelight.cpp
@@ -428,7 +428,7 @@ std::optional<units::length::inch_t> DragonLimelight::EstimateTargetXDistance()
             else
             {
                 // TODO come back to this with different math
-                // units::length::inch_t estimatedTargetDistance = (m_noteVerticalOffset - mountingHeight) / tangent;
+                units::length::inch_t estimatedTargetDistance = mountingHeight / tangent;
 
                 return estimatedTargetDistance;
             }
@@ -472,7 +472,7 @@ std::optional<units::length::inch_t> DragonLimelight::EstimateTargetZDistance()
     if (!GetAprilTagID())
     {
         // TODO COME BACK TO THIS ONE
-        // units::length::inch_t estimatedTargetZDistance = m_cameraPose.Z() - m_noteVerticalOffset;
+        units::length::inch_t estimatedTargetZDistance = m_cameraPose.Z();
         return estimatedTargetZDistance;
     }
 
