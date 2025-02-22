4284a30fba426cf9b36b04008598a05564e90476
reslip <reslip@icloud.com>
2025-02-22 09:43:20 -0500

    keep mechanism config

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -224,6 +224,7 @@ void Robot::SimulationPeriodic()
 void Robot::InitializeRobot()
 {
     int32_t teamNumber = frc::RobotController::GetTeamNumber();
+    MechanismConfigMgr::GetInstance()->InitRobot((RobotIdentifier)teamNumber);
     ChassisConfigMgr::GetInstance()->InitChassis(static_cast<RobotIdentifier>(teamNumber));
     auto chassisConfig = ChassisConfigMgr::GetInstance()->GetCurrentConfig();
     m_chassis = chassisConfig != nullptr ? chassisConfig->GetSwerveChassis() : nullptr;
