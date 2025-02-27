e0e35e7962e105dd4d33f05b35a9e1874d04e7dd
Tan560 <97492534+Tan560@users.noreply.github.com>
2025-02-27 09:01:42 -0500

    Updating Code Generator and Learning from Drive Team

--- src/main/cpp/mechanisms/DragonTale/ScoreCoralState.h
+++ src/main/cpp/mechanisms/DragonTale/ScoreCoralState.h
@@ -48,6 +48,7 @@ namespace DragonTaleStates
 		const units::angle::degree_t m_ArmTarget = units::angle::degree_t(0);
 		const units::length::inch_t m_ElevatorLeaderTarget = units::length::inch_t(0);
 		const double m_CoralTarget = double(0.5);
+		const double m_CoralL1Target = double(0.2);
 		const double m_AlgaeTarget = double(0);
 	};
 }
