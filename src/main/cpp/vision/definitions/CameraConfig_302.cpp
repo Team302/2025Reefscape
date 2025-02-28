cdab6ea3ddf6dac7d9bde0f450759c05f77b3703
notcharlie2026 <118229690+notcharlie2026@users.noreply.github.com>
2025-02-27 19:30:57 -0500

    Update CameraConfig_302.cpp

--- src/main/cpp/vision/definitions/CameraConfig_302.cpp
+++ src/main/cpp/vision/definitions/CameraConfig_302.cpp
@@ -43,10 +43,10 @@ void CameraConfig_302::BuildCameraConfig()
                                                 DRAGON_LIMELIGHT_CAMERA_IDENTIFIER::BACK_CAMERA,
                                                 DRAGON_LIMELIGHT_CAMERA_TYPE::LIMELIGHT4,  // PIPELINE initialPipeline,
                                                 DRAGON_LIMELIGHT_CAMERA_USAGE::APRIL_TAGS, // PIPELINE initialPipeline,
-                                                units::length::meter_t(0.2032),            // units::length::inch_t mountingXOffset, /// <I> x offset of cam from robot center (forward relative to robot)
-                                                units::length::meter_t(-0.0889),           // units::length::inch_t mountingYOffset, /// <I> y offset of cam from robot center (left relative to robot)
-                                                units::length::meter_t(0.7874),            // units::length::inch_t mountingZOffset, /// <I> z offset of cam from robot center (up relative to robot)
-                                                units::angle::degree_t(0),                 // units::angle::degree_t pitch,          /// <I> - Pitch of camera
+                                                units::length::meter_t(-0.2159),           // units::length::inch_t mountingXOffset, /// <I> x offset of cam from robot center (forward relative to robot)
+                                                units::length::meter_t(-0.2286),           // units::length::inch_t mountingYOffset, /// <I> y offset of cam from robot center (left relative to robot)
+                                                units::length::meter_t(0.6096),            // units::length::inch_t mountingZOffset, /// <I> z offset of cam from robot center (up relative to robot)
+                                                units::angle::degree_t(90),                // units::angle::degree_t pitch,          /// <I> - Pitch of camera
                                                 units::angle::degree_t(0),                 // units::angle::degree_t yaw,            /// <I> - Yaw of camera
                                                 units::angle::degree_t(0),                 // units::angle::degree_t roll,           /// <I> - Roll of camera
                                                 DRAGON_LIMELIGHT_PIPELINE::APRIL_TAG,      /// <I> enum for starting pipeline
