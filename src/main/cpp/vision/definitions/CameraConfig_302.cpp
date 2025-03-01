b8d2713e44b842ff302ab3755fb18f8ed28027e5
notcharlie2026 <118229690+notcharlie2026@users.noreply.github.com>
2025-03-01 09:34:05 -0500

    coral changes

--- src/main/cpp/vision/definitions/CameraConfig_302.cpp
+++ src/main/cpp/vision/definitions/CameraConfig_302.cpp
@@ -46,7 +46,7 @@ void CameraConfig_302::BuildCameraConfig()
                                                 units::length::meter_t(-0.2159),           // units::length::inch_t mountingXOffset, /// <I> x offset of cam from robot center (forward relative to robot)
                                                 units::length::meter_t(0.2286),            // units::length::inch_t mountingYOffset, /// <I> y offset of cam from robot center (left relative to robot)
                                                 units::length::meter_t(0.6096),            // units::length::inch_t mountingZOffset, /// <I> z offset of cam from robot center (up relative to robot)
-                                                units::angle::degree_t(90),                // units::angle::degree_t pitch,          /// <I> - Pitch of camera
+                                                units::angle::degree_t(-90),               // units::angle::degree_t pitch,          /// <I> - Pitch of camera
                                                 units::angle::degree_t(0),                 // units::angle::degree_t yaw,            /// <I> - Yaw of camera
                                                 units::angle::degree_t(0),                 // units::angle::degree_t roll,           /// <I> - Roll of camera
                                                 DRAGON_LIMELIGHT_PIPELINE::APRIL_TAG,      /// <I> enum for starting pipeline
