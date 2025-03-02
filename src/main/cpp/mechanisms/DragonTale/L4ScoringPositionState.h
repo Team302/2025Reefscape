51d84ffdc698e5cb935dc3cf20b7e553a3ed68d7
JamesLimothy <118229779+JamesLimothy@users.noreply.github.com>
2025-03-01 20:57:10 -0500

    day1 comp thingys

--- src/main/cpp/mechanisms/DragonTale/L4ScoringPositionState.h
+++ src/main/cpp/mechanisms/DragonTale/L4ScoringPositionState.h
@@ -46,7 +46,7 @@ namespace DragonTaleStates
 		void InitCOMP_BOT302();
 		RobotIdentifier m_RobotId;
 		const units::angle::degree_t m_ArmTarget = units::angle::degree_t(51.5);
-		const units::length::inch_t m_ElevatorLeaderTarget = units::length::inch_t(29);
+		const units::length::inch_t m_ElevatorLeaderTarget = units::length::inch_t(29.5);
 		const double m_CoralTarget = double(0);
 		const double m_AlgaeTarget = double(0);
 	};
