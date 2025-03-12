4ae02a645cef4afcb19c0f8041e688e51e1268b4
Aden25 <loslatterya55@stu.lok12.org>
2025-03-11 20:17:00 -0400

    all good 👍

--- src/main/cpp/chassis/states/DriveToFieldElement.h
+++ src/main/cpp/chassis/states/DriveToFieldElement.h
@@ -64,9 +64,9 @@ private:
 
     const units::length::inch_t m_distanceThreshold{0.25};
     const units::length::meter_t m_ffMinRadius{0.05};
-    const units::length::meter_t m_ffMaxRadius{1.0};
+    const units::length::meter_t m_ffMaxRadius{2.0};
 
-    const units::velocity::meters_per_second_t kMaxVelocity = 3.5_mps;
+    const units::velocity::meters_per_second_t kMaxVelocity = 2.5_mps;
     const units::acceleration::meters_per_second_squared_t kMaxAcceleration = 1.5_mps_sq;
 
     const units::angular_velocity::degrees_per_second_t kMaxAngularVelocity = 540_deg_per_s;
@@ -74,8 +74,8 @@ private:
     units::time::second_t m_lastResetTime = 0_s;
     units::time::second_t m_resetTime = 0.5_s;
 
-    const double m_translationKP = 10.0;
-    const double m_translationKI = 2.5;
+    const double m_translationKP = 6.0;
+    const double m_translationKI = 4;
     const double m_translationKD = 0.0;
 
     const double m_rotationKP = 6.0;
