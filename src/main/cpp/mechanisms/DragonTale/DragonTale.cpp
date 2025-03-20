d34ef301249717dee0e5202524fd998ef6890667
JamesLimothy <118229779+JamesLimothy@users.noreply.github.com>
2025-03-19 20:23:15 -0400

    gear rat 👍

--- src/main/cpp/mechanisms/DragonTale/DragonTale.cpp
+++ src/main/cpp/mechanisms/DragonTale/DragonTale.cpp
@@ -915,7 +915,7 @@ void DragonTale::InitializeTalonFXElevatorLeaderCOMP_BOT302()
 	configs.Feedback.SensorToMechanismRatio = 0.134535;
 
 	m_elevatorDiameterInch = 0.75;
-	m_elevatorGearRatio = 9.0;
+	m_elevatorGearRatio = 4.0;
 
 	configs.Slot0.kP = m_PositionInch->GetP();
 	configs.Slot0.kI = m_PositionInch->GetI();
