927f7aace38f0b0739b83d4196a74c5903e1cc1f
Tan560 <97492534+Tan560@users.noreply.github.com>
2025-03-25 17:15:29 -0400

    Cleaned up all transition conditions

--- src/main/cpp/mechanisms/DragonTale/GrabAlgaeFloorState.cpp
+++ src/main/cpp/mechanisms/DragonTale/GrabAlgaeFloorState.cpp
@@ -86,5 +86,5 @@ bool GrabAlgaeFloorState::IsTransitionCondition(bool considerGamepadTransitions)
 {
 	// To get the current state use m_mechanism->GetCurrentState()
 
-	return (considerGamepadTransitions && (TeleopControl::GetInstance()->IsButtonPressed(TeleopControlFunctions::ALGAE_INTAKE)) && !m_mechanism->GetAlgaeSensorState() && !m_mechanism->GetManualMode());
+	return (considerGamepadTransitions && (TeleopControl::GetInstance()->IsButtonPressed(TeleopControlFunctions::ALGAE_INTAKE)) && !m_mechanism->GetAlgaeSensorState());
 }
