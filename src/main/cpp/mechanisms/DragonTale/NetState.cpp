ab0b4221b3cedbaa36eeec76a8bde86ff6783c1e
Joe Witcpalek <joe.witcpalek@gmail.com>
2025-04-02 21:04:12 -0400

    Working changes from drive practice

--- src/main/cpp/mechanisms/DragonTale/NetState.cpp
+++ src/main/cpp/mechanisms/DragonTale/NetState.cpp
@@ -86,5 +86,5 @@ bool NetState::IsTransitionCondition(bool considerGamepadTransitions)
 {
 	// To get the current state use m_mechanism->GetCurrentState()
 
-	return (((m_mechanism->GetAlgaeSensorState() && m_mechanism->IsInBargeZone()) || (TeleopControl::GetInstance()->IsButtonPressed(TeleopControlFunctions::FORCE_ELEVATOR) && m_mechanism->IsAlgaeMode())) && considerGamepadTransitions && TeleopControl::GetInstance()->IsButtonPressed(TeleopControlFunctions::L4_SCORING_POSITION));
+	return (((m_mechanism->IsInBargeZone()) || (TeleopControl::GetInstance()->IsButtonPressed(TeleopControlFunctions::FORCE_ELEVATOR) && m_mechanism->IsAlgaeMode())) && considerGamepadTransitions && TeleopControl::GetInstance()->IsButtonPressed(TeleopControlFunctions::L4_SCORING_POSITION));
 }
