dbea67d491f5965a356ce843e386a934991a5298
Tan560 <97492534+Tan560@users.noreply.github.com>
2025-04-14 20:06:57 -0400

    Deleted Comments

--- src/main/cpp/chassis/states/DriveToFieldElement.cpp
+++ src/main/cpp/chassis/states/DriveToFieldElement.cpp
@@ -106,20 +106,6 @@ std::array<frc::SwerveModuleState, 4> DriveToFieldElement::UpdateSwerveModuleSta
             CalculateFeedForward(chassisMovement);
             auto chassisSpeeds = chassisMovement.chassisSpeeds;
 
-            // units::time::second_t currentTime = frc::Timer::GetFPGATimestamp();
-
-            // // Reset the PID if resetTime in second has passed since the last reset
-            // if (m_resetTime <= (currentTime - m_lastResetTime))
-            // {
-            //     m_translationPIDX.Reset(m_currentPose.X(), chassisMovement.chassisSpeeds.vx);
-            //     m_translationPIDY.Reset(m_currentPose.Y(), chassisMovement.chassisSpeeds.vy);
-            //     m_lastResetTime = currentTime;
-            // }
-            // else
-            // {
-            //     m_lastResetTime = currentTime;
-            // }
-
             auto info = DragonTargetFinder::GetInstance()->GetPose(GetDriveToTarget());
             if (info.has_value())
             {
