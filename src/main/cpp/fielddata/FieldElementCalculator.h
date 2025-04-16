2288d7ae8dabc611ebca1ba555fb30e08e43ee94
JamesLimothy <118229779+JamesLimothy@users.noreply.github.com>
2025-04-16 16:03:57 -0400

    climb/barge/center auto changes

--- src/main/cpp/fielddata/FieldElementCalculator.h
+++ src/main/cpp/fielddata/FieldElementCalculator.h
@@ -138,14 +138,14 @@ private:
 
     frc::Transform3d m_calcBargeFront = frc::Transform3d(
         frc::Translation3d(
-            units::length::inch_t(18.0), // 16.0
+            units::length::inch_t(20.0), // 16.0
             units::length::inch_t(0.0),
             units::length::inch_t(0.0)),
         frc::Rotation3d());
 
     frc::Transform3d m_calcBargeBack = frc::Transform3d(
         frc::Translation3d(
-            units::length::inch_t(19.0), // 16.0
+            units::length::inch_t(20.0), // 16.0
             units::length::inch_t(0.0),
             units::length::inch_t(0.0)),
         frc::Rotation3d());
