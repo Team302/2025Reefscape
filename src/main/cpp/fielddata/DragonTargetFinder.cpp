51b46759d34e791a3b65b9b604c23324804cb3fa
Tan560 <97492534+Tan560@users.noreply.github.com>
2025-07-17 22:39:40 -0400

    Fixed AutoGrabAlgae Position

--- src/main/cpp/fielddata/DragonTargetFinder.cpp
+++ src/main/cpp/fielddata/DragonTargetFinder.cpp
@@ -76,8 +76,7 @@ optional<tuple<DragonTargetFinderData, Pose2d>> DragonTargetFinder::GetPose(Drag
 
             if (item == DragonTargetFinderTarget::CLOSEST_REEF_ALGAE)
             {
-                auto targetAngle = FMSData::GetAllianceColor() == frc::DriverStation::Alliance::kBlue ? (tagpose.Rotation().Degrees() + 180_deg) : tagpose.Rotation().Degrees(); // TODO: Investigate why we need to add 180 degrees for blue alliance (write story)
-                m_goalPose = frc::Pose2d(tagpose.X(), tagpose.Y(), targetAngle);
+                m_goalPose = tagpose;
                 return make_tuple(DragonTargetFinderData::ODOMETRY_BASED, m_goalPose.value());
             }
             else if (item == DragonTargetFinderTarget::CLOSEST_LEFT_REEF_BRANCH)
