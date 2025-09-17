df46b59e472e693d730ad69b57387060ad1dea24
Tan560 <97492534+Tan560@users.noreply.github.com>
2025-09-17 19:21:10 -0400

    Updated Climber

--- src/main/cpp/mechanisms/ClimberManager/ClimberManager.cpp
+++ src/main/cpp/mechanisms/ClimberManager/ClimberManager.cpp
@@ -288,7 +288,7 @@ void ClimberManager::InitializeTalonFXClimberPRACTICE_BOT9999()
 	configs.MotorOutput.DutyCycleNeutralDeadband = 0;
 
 	configs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue::RotorSensor;
-	configs.Feedback.SensorToMechanismRatio = 1.0495;
+	configs.Feedback.SensorToMechanismRatio = 1.0405;
 
 	configs.Slot0.kP = m_PositionDegree->GetP();
 	configs.Slot0.kI = m_PositionDegree->GetI();
